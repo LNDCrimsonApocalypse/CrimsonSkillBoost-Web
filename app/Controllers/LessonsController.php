1b038272973a3768c584a9d141fac9f6566ab664
FlamerYeeter <86110168+FlamerYeeter@users.noreply.github.com>
2025-05-22 13:52:44 +0800

    Revert "Added Lesson View for Website"
    This reverts commit 720a72490c4d6749265212bdb8aae6dbbb113fd6.

--- app/Controllers/LessonsController.php
+++ app/Controllers/LessonsController.php
@@ -16,29 +16,4 @@ class LessonsController extends BaseController
 
         return view('dashboard', $data);
     }
-    public function edit($id)
-    {
-        $model = new LessonModel();
-        $lesson = $model->find($id);
-
-        if (!$lesson) {
-            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Lesson not found.");
-        }
-
-        return view('lessons/edit', ['lesson' => $lesson]);
-    }
-
-    public function update($id)
-    {
-        $model = new LessonModel();
-
-        $data = [
-            'title'   => $this->request->getPost('title'),
-            'content' => $this->request->getPost('content'),
-        ];
-
-        $model->update($id, $data);
-
-        return redirect()->to('/dashboard')->with('success', 'Lesson updated successfully');
-    }
 }
